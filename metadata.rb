94d0b4fcc9bfb6f61574340462dedd38d893ff75
Mawi Marín <mawitu@gmail.com>
2011-12-27 18:09:57 +0100

    Adding homepage attribute

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,11 @@ description       "Change your browser homepage"
 version           "0.1.0"
 supports          "homepage"
 recipe            "homepage", "Change your browser homepage"
+
+
+attribute 'homepage/homepage',
+  :display_name => "Homepage",
+  :description  => "The web page to be set as a Firefox's homepage",
+  :type         => "string",
+  :required     => "required",
+  :recipes      => [ 'homepage::homepage' ]
